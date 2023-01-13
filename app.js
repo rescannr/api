89bf6e7c787ff5c56b950dcf45791351780eb210
Aleksej Komnenović <dev@akom.me>
2023-01-13 18:59:12 +0100

    add swagger ui setup

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 const port = 3000;
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");

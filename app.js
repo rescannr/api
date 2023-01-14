3982a833edae81617df7f4e7ef8a123d5d571f08
Aleksej Komnenović <dev@akom.me>
2023-01-14 17:34:54 +0100

    include router in main file

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
+const invoice_scanning_router = require("./routes/invoice_scanning");
+
 const app = express();
 const port = 3000;
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/inv_scn", invoice_scanning_router);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");

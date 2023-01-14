bb5acfec5e2e7fc4fe5e25e4c8b7d09845fb6e5a
Aleksej Komnenović <dev@akom.me>
2023-01-14 17:34:37 +0100

    fix router

--- routes/invoice_scanning.js
+++ routes/invoice_scanning.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(express.json({ extended: true }));
+
 router.post("/", (req, res) => {
   // TODO: logging
+
+  console.log(req.body);
 });
 
 module.exports = router;

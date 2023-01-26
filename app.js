d9a6796f029aa499e7e776eee98f903a793e747e
Aleksej Komnenović <dev@akom.me>
2023-01-26 22:32:13 +0100

    set express-session with env var

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+const session = require("express-session");
+require("dotenv").config();
 
 const receipt_scanning_router = require("./routes/receipt_scanning");
 
 const app = express();
 const port = 3000;
 
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/inv_scn", receipt_scanning_router);
 

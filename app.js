0a6671548c6592d8500e3038b545b5a5c838187a
Aleksej Komnenović <dev@akom.me>
2023-01-28 18:34:58 +0100

    connect to mongodb, update port

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 const session = require("express-session");
 const passport = require("passport");
+const connectMongoDB = require("./db/mongodb.js");
 require("dotenv").config();
 
 const receipt_scanning_router = require("./routes/receipt_scanning");
 
 const app = express();
-const port = 3000;
+const port = process.env.EXPRESS_PORT;
 
 app.use(
   session({
@@ -26,6 +27,8 @@ app.use("/inv_scn", receipt_scanning_router);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+connectMongoDB();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

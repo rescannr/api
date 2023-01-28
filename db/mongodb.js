be617dd02308b4955956051382f6b46c4c1e3074
Aleksej Komnenović <dev@akom.me>
2023-01-29 00:23:25 +0100

    add db name

--- db/mongodb.js
+++ db/mongodb.js
@@ -6,7 +6,11 @@ mongoose.Promise = global.Promise;
 const dbUrl = process.env.DB_URL;
 
 const connect = async () => {
-  mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "rescannr",
+  });
   const db = mongoose.connection;
   db.on("error", () => {
     console.log("could not connect");

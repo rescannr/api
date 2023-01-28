a74db3e8e49d67080586eda1d7027971332b689a
Aleksej Komnenović <dev@akom.me>
2023-01-28 18:51:23 +0100

    add login strategy for auth, with mongodb checks

--- middleware/passport/passportConfig.js
+++ middleware/passport/passportConfig.js
@@ -23,4 +23,31 @@ module.exports = (passport) => {
       }
     )
   );
+
+  passport.use(
+    "local-login",
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+      },
+      async (email, password, done) => {
+        try {
+          const user = await User.findOne({ email: email });
+
+          if (!user) return done(null, false);
+
+          const isMatch = await user.matchPassword(password);
+
+          if (!isMatch) return done(null, false);
+
+          return done(null, user);
+        } catch (error) {
+          console.log(error);
+
+          return done(error, false);
+        }
+      }
+    )
+  );
 };

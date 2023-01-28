7601c4280374dd868fc8d3449109b1c053fda582
Aleksej Komnenović <dev@akom.me>
2023-01-28 16:59:31 +0100

    create user schema and export it as a model

---
+++ db/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = mongoose.model("user", UserSchema);
+module.exports = UserModel;

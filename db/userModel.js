8bf6351fa242b14bab62827109656f0b661c1eef
Aleksej Komnenović <dev@akom.me>
2023-01-28 17:07:09 +0100

    add a pre hook, to modify password to hash using bcrypt

--- db/userModel.js
+++ db/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const { Schema } = mongoose;
+
 const UserSchema = new Schema({
   email: {
     type: String,
@@ -9,6 +11,19 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+UserSchema.pre("save", async function (next) {
+  try {
+    const user = this;
+    if (!user.isModified("password")) next();
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 const UserModel = mongoose.model("user", UserSchema);

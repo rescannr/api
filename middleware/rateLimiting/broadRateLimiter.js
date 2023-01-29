0af0f5b979c1feb2f35351a1e60ecdcfd150cdc7
Aleksej Komnenović <dev@akom.me>
2023-01-29 18:55:48 +0100

    add rate limiter

---
+++ middleware/rateLimiting/broadRateLimiter.js
@@ -0,0 +1,11 @@
+import rateLimit from "express-rate-limit";
+
+let limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: "Too many requests from this IP, please try again later.",
+});
+
+module.exports = limiter;

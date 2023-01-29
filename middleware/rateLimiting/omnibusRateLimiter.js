8ce4dcc2628d6abc71701b8251c9ecf4edc3c4cc
Aleksej Komnenović <dev@akom.me>
2023-01-29 19:03:14 +0100

    update name and import of npm package

--- middleware/rateLimiting/omnibusRateLimiter.js
+++ middleware/rateLimiting/omnibusRateLimiter.js
@@ -1,8 +1,8 @@
-import rateLimit from "express-rate-limit";
+const rateLimit = require("express-rate-limit");
 
 let limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 100,
+  max: 10,
   standardHeaders: true,
   legacyHeaders: false,
   message: "Too many requests from this IP, please try again later.",

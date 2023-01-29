a52b7b4a06eb42e55f3c4be84efbbb4507d13897
Aleksej Komnenović <dev@akom.me>
2023-01-29 17:18:01 +0100

    add de/serialisation for session

--- middleware/passport/passportConfig.js
+++ middleware/passport/passportConfig.js
@@ -2,53 +2,69 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("../../db/userModel.js");
 
-module.exports = () => {
-  passport.use(
-    "local-signup",
-    new LocalStrategy(
-      {
-        usernameField: "email",
-        passwordField: "password",
-      },
-      async (email, password, done) => {
-        try {
-          const userExists = await User.findOne({ email: email });
-          if (userExists) {
-            return done(null, false);
-          }
-          const user = await User.create({ email, password });
-          return done(null, user);
-        } catch (error) {
-          done(error);
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+    return done(null, user);
+  });
+});
+
+passport.use(
+  "local-signup",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const userExists = await User.findOne({ email: email });
+        if (userExists) {
+          return done(null, false);
         }
+        const user = await User.create({ email, password });
+        return done(null, user);
+      } catch (error) {
+        done(error);
       }
-    )
-  );
+    }
+  )
+);
 
-  passport.use(
-    "local-login",
-    new LocalStrategy(
-      {
-        usernameField: "email",
-        passwordField: "password",
-      },
-      async (email, password, done) => {
-        try {
-          const user = await User.findOne({ email: email });
+passport.use(
+  "local-login",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email: email });
 
-          if (!user) return done(null, false);
+        if (!user) return done(null, false);
 
-          const isMatch = await user.matchPassword(password);
+        const isMatch = await user.matchPassword(password);
 
-          if (!isMatch) return done(null, false);
+        if (!isMatch) return done(null, false);
 
-          return done(null, user);
-        } catch (error) {
-          console.log(error);
+        return done(null, user);
+      } catch (error) {
+        console.log(error);
 
-          return done(error, false);
-        }
+        return done(error, false);
       }
-    )
-  );
-};
+    }
+  )
+);
+
+module.exports = () => passport;

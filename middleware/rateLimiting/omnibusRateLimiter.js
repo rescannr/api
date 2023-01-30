df6e6f61b33ca1f07d87944ebceb477bf8926c6d
Aleksej Komnenović <dev@akom.me>
2023-01-30 20:49:32 +0100

    update rate limiter

--- middleware/rateLimiting/omnibusRateLimiter.js
+++ middleware/rateLimiting/omnibusRateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 let limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 10,
+  max: 100,
   standardHeaders: true,
   legacyHeaders: false,
   message: "Too many requests from this IP, please try again later.",
